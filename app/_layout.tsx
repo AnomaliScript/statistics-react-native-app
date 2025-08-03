b0c2be144948eea4654375fdd6669650db04776b
AnomaliScript <anomaliscript@gmail.com>
2025-08-03 10:13:13 -0400

    i figured it out! (8-3)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,24 @@
-import { Stack, useRouter } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
+import { useAuth } from "../lib/auth-context";
 
 function RouteGuard({ children } : { children: React.ReactNode }) {
 
   const router = useRouter();
-  const isAuth = false;
+  const { user, isLoadingUser } = useAuth();
+  const segments = useSegments();
 
   useEffect(() => {
-    if (!isAuth) {
+    const inAuthGroup = segments[0] === "auth"
+
+    if (!user && !inAuthGroup && !isLoadingUser) {
+      // If user is not authenticated and is not in the login (auth) screen
       router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      // If user exists and is not in the login (auth) screen
+      router.replace("/")
     }
-  }, [isAuth, router]);
+  }, [user, segments]);
 
   return <>{children}</>
 }
